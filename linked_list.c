d8eff23b126b8d441e76664e1c1f7a940f5ab689
root <root@Dilny-HP>
2025-12-05 12:38:31 +0100

    Revert "feat: test size func"
    This reverts commit 0697d71d7be02936a2b07acb802e3423520c54c7.

--- linked_list.c
+++ linked_list.c
@@ -111,6 +111,8 @@ size_t linked_list_size(node *me) {
 
     while(current != NULL) {
 
+        size++;
+
         current = current->next;
 
     }
--- test/unit_test.c
+++ test/unit_test.c
@@ -83,27 +83,10 @@ void test_function_remove(void) {
 
 }
 
-void test_function_size(void) {
-
-    size_t size = linked_list_size(test_head);
-
-    TEST_ASSERT_EQUAL_size_t(0, size);
-
-    bool success = linked_list_add(&test_head, 7);
-
-    TEST_ASSERT_TRUE(success);
-
-    size = linked_list_size(test_head);
-
-    TEST_ASSERT_EQUAL_size_t(1, size);
-
-}
-
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_function_destroy);
     RUN_TEST(test_function_add);
     RUN_TEST(test_function_remove);
-    RUN_TEST(test_function_size);
     return UNITY_END();
 }
